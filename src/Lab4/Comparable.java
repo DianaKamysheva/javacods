f9577feacb22a6f50f8bc2f2a3c0aed547dbbda9
Диана <kamychevada@gmail.com>
2024-11-26 01:33:51 +0500

    Обновление

--- src/Lab4/Comparable.java
+++ src/Lab4/Comparable.java
@@ -1,44 +1,52 @@
 package Lab4;
 
-// Интерфейс Сравнимое
-public interface Comparable<T> {
-    int compare(T obj);
+import java.util.Scanner;
+
+// Интерфейс Comparable
+interface Comparable {
+    int compare(Object obj);
 }
 
-// Класс, реализующий интерфейс Сравнимое
-class Number implements Comparable<Number> {
+// Класс для задачи 1
+class Task1 implements Comparable {
     private int value;
 
-    public Number(int value) {
+    public Task1(int value) {
         this.value = value;
     }
 
     @Override
-    public int compare(Number obj) {
-        return Integer.compare(this.value, obj.value);
+    public int compare(Object obj) {
+        if (obj instanceof Task1) {
+            return Integer.compare(this.value, ((Task1) obj).value);
+        }
+        return -1;
     }
 
     @Override
     public String toString() {
-        return "Число: " + value;
+        return "Task1{value =" + value + "}";
     }
 }
 
-// Класс, реализующий интерфейс Сравнимое
-class StringType implements Comparable<StringType> {
-    private String value;
+// Класс для задачи 2
+class Task2 implements Comparable {
+    private String string;
 
-    public StringType(String value) {
-        this.value = value;
+    public Task2(String string) {
+        this.string = string;
     }
 
     @Override
-    public int compare(StringType obj) {
-        return this.value.compareTo(obj.value);
+    public int compare(Object obj) {
+        if (obj instanceof Task2) {
+            return this.string.compareTo(((Task2) obj).string);
+        }
+        return -1;
     }
 
     @Override
     public String toString() {
-        return "Строка: " + value;
+        return "Task2{string =" + string + "}";
     }
 }
